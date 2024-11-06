a8af8f49160a6a12a7dde7133ae4b333ac5979e8
sunnat <sunnat@gmail.com>
2024-11-06 14:09:48 +0300

    that is all

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ function App() {
         <Route path="/portfolio/contacts" element={<Contacts />} />
         <Route path="/*" element={<NotFound></NotFound>} />
       </Routes>
+      <Footer/>
     </>
   );
 }
